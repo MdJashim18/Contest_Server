682bbe92907e7001fa4f2947c34cd53d39426e2e
MdJashim18 <mdjashimuddinjnn22990@gmail.com>
2025-12-14 12:25:39 +0600

    third commit

--- index.js
+++ index.js
@@ -1,43 +1,108 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000;
 
-
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mongo-simple-crud.tzwys72.mongodb.net/?appName=Mongo-simple-crud`;
-
-
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+
+        const db = client.db("contestHub");
+        const usersCollection = db.collection("users");
+        const contestCollection = db.collection("contest")
+
+        // ================= POST new user =================
+        app.post("/users", async (req, res) => {
+            const user = req.body
+            user.role = "user"
+            user.createdAt = new Date()
+
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        });
+        app.get("/users", async (req, res) => {
+            const cursor = usersCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.patch('/users/:id', async (req, res) => {
+            const { id } = req.params;
+            const { role } = req.body;
+            const result = await usersCollection.updateOne(
+                { _id: new ObjectId(id) },
+                { $set: { role } }
+            );
+            res.send(result);
+        });
+
+
+
+
+        app.post("/contest", async (req, res) => {
+            const contest = req.body;
+            contest.status = "pending";
+            contest.participants = 0;
+            contest.createdAt = new Date();
+            const result = await contestCollection.insertOne(contest);
+            res.send(result);
+        })
+        app.get("/contest", async (req, res) => {
+            const cursor = contestCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        });
+        app.get("/contest/:id", async (req, res) => {
+            const contest = await contestCollection.findOne({
+                _id: new ObjectId(req.params.id),
+            });
+            res.send(contest);
+        });
+        app.patch("/contest/approve/:id", async (req, res) => {
+            const result = await contestCollection.updateOne(
+                { _id: new ObjectId(req.params.id) },
+                { $set: { status: "approved" } }
+            );
+            res.send(result);
+        }
+        );
+        app.patch("/contest/reject/:id", async (req, res) => {
+            const result = await contestCollection.updateOne(
+                { _id: new ObjectId(req.params.id) },
+                { $set: { status: "rejected" } }
+            );
+            res.send(result);
+        }
+        );
+        app.delete("/contest/:id",async (req, res) => {
+                const result = await contestCollection.deleteOne({
+                    _id: new ObjectId(req.params.id),
+                });
+                res.send(result);
+            }
+        );
+
+
+    } finally {
+        // Do not close client to keep server running
+    }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Contest Hub')
-})
+    res.send('Contest Hub API');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server running on port ${port}`);
+});
